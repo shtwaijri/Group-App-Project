5f62271382a8d665e8426a16057afaa09c5c1812
Shatha 01 <shtwaijri>
2025-06-19 11:37:01 +0300

    updated folders arrangement

---
+++ essert_coffee/lib/features/branches/screens/branches_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class BranchesScreen extends StatelessWidget {
+  const BranchesScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Center(child: Text('Branches screen')));
+  }
+}
